aa17e6fbea3d103ce33d336906f5439d7a172888
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-09-24 15:18:16 -0300

    Test

--- src/main/java/br/com/alura/loja/Produto.java
+++ src/main/java/br/com/alura/loja/Produto.java
@@ -1,5 +1,28 @@
 package br.com.alura.loja;
 
+import java.math.BigDecimal;
+
 public class Produto {
+private String nome;
+private BigDecimal preco;
+public Produto(String nome, BigDecimal preco) {
+	super();
+	this.nome = nome;
+	this.preco = preco;
+}
+public String getNome() {
+	return nome;
+}
+public BigDecimal getPreco() {
+	return preco;
+}
+public void setPreco(BigDecimal preco) {
+	this.preco = preco;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+
 
 }
--- src/test/java/br/com/alura/loja/ProdutoTest.java
+++ src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,13 +1,18 @@
 package br.com.alura.loja;
 
-import static org.junit.Assert.*;
+import java.math.BigDecimal;
 
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
+		Produto p = new Produto("teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", p.getNome());
+		Assert.assertEquals(BigDecimal.TEN, p.getPreco());
 		
 	}
 
